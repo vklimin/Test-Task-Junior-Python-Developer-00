c8de139aa76a78bf40337255de33995f40b06d56
Виталий Г. Климин <minggamo@student.21-school.ru>
2025-09-01 07:44:51 +0300

    Создание точки входа

---
+++ app/main.py
@@ -0,0 +1,19 @@
+# app/main.py
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from app import models, database
+
+app = FastAPI(title="Тестовое задание: API-сервис для вопросов и ответов")
+
+# Зависимость для сессии БД
+def get_db():
+    db = database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, world!"}
+
