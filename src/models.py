84d2f6be3407f1a49483afcbc98271178384514f
Виталий Г. Климин <minggamo@student.21-school.ru>
2025-09-02 08:17:51 +0300

    Добавлены таблицы Вопросов и Ответов

--- src/models.py
+++ src/models.py
@@ -1,4 +1,35 @@
 # src/models.py
-from sqlalchemy import Column, Integer, String, DateTime, func
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, func
+from sqlalchemy.orm import relationship
 from src.database import Base
 
+
+"""
+Логика:
+* Нельзя создать ответ к несуществующему вопросу.
+* Один и тот же пользователь может оставлять несколько ответов на один вопрос.
+* При удалении вопроса должны удаляться все его ответы (каскадно).
+"""
+
+class Question(Base):
+    __tablename__ = "questions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    text = Column(String, nullable=False) # Текст вопроса
+    created_at = Column(DateTime(timezone=True), default=func.now(), nullable=False)
+
+    # Один вопрос - много ответов
+    answers = relationship("Answer", back_populates="question", cascade="all, delete-orphan", lazy="select")
+
+
+class Answer(Base):
+    __tablename__ = "answers"
+
+    id = Column(Integer, primary_key=True, index=True)
+    question_id = Column(Integer, ForeignKey("questions.id", ondelete="CASCADE"), nullable=False)
+    user_id = Column(String, nullable=False)  # UUID пользователя
+    text = Column(String, nullable=False)     # Текст ответа
+    created_at = Column(DateTime(timezone=True), default=func.now(), nullable=False)
+
+    # Обратная связь к вопросу
+    question = relationship("Question", back_populates="answers")
