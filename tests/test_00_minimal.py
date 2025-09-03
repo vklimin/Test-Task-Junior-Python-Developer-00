f1d07c023f432bf77fc46e41e718d14c205938e4
Виталий Г. Климин <minggamo@student.21-school.ru>
2025-09-03 18:21:05 +0300

    Добавление первого теста

---
+++ tests/test_00_minimal.py
@@ -0,0 +1,11 @@
+# tests/test_00_minimal.py
+from fastapi.testclient import TestClient
+from src.main import app
+
+client = TestClient(app)
+
+def test_read_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello, world!"}
+
