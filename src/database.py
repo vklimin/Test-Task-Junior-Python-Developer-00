aebbd27685eb879a984a776201c83b71aaacf34c
Виталий Г. Климин <minggamo@student.21-school.ru>
2025-09-03 18:20:29 +0300

    Исправление depricated функций

--- src/database.py
+++ src/database.py
@@ -1,7 +1,6 @@
 # src/database.py
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
--- src/schemas.py
+++ src/schemas.py
@@ -2,7 +2,7 @@
 """
 Pydantic-схемы для FastAPI
 """
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from datetime import datetime
 
 
@@ -21,5 +21,4 @@ class QuestionResponse(BaseModel):
     text: str
     created_at: datetime
 
-    class Config:
-        from_attributes = True
+    model_config = ConfigDict(from_attributes=True)
